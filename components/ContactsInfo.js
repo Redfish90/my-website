3faeb15ad8f2144dfd75c29f43e80720e7cd3edb
crentifis <redifischris@gmail.com>
2024-09-13 13:18:50 +0300

    Add EmailJS

--- components/ContactsInfo.js
+++ components/ContactsInfo.js
@@ -1,4 +1,58 @@
+'use client'
+
+import * as emailjs from '@emailjs/browser'
+import { useState } from 'react'
+
 const ContactsInfo = () => {
+  // State to capture form data
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  // Handle input change
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+// Function to send email
+  const sendEmail = (e) => {
+    e.preventDefault(); // Corrected typo here
+
+    const serviceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
+    const templateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
+    const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
+
+    // Create a new object that contains dynamic template params
+    const templateParams = {
+      to_name: 'Rentifis Christoforos', // You can customize this
+      from_name: formData.name,
+      from_email: formData.email,
+      message: formData.message,
+    };
+
+    // Send the email using EmailJS
+    emailjs
+        .send(serviceId, templateId, templateParams, publicKey)
+        .then((response) => {
+          console.log("Email sent successfully!", response);
+          // Optionally reset form fields here
+          setFormData({ name: '', email: '', message: '' });
+
+          // Scroll to the top of the page
+          window.scrollTo({
+            top: 0,
+            behavior: 'smooth' // This adds a smooth scrolling effect
+          });
+        })
+        .catch((error) => {
+          console.error("Error sending email:", error);
+        });
+  };
+
+
   return (
     <div className="section contacts section_" id="section-contacts">
       <div className="content">
@@ -33,23 +87,24 @@ const ContactsInfo = () => {
           </div>
         </div>
         <div className="contact_form">
-          <form id="cform" method="post">
+          <form id="cform" onSubmit={sendEmail}>
             <div className="group-val">
-              <input type="text" name="name" placeholder="Name" />
+              <input type="text" name="name" placeholder="Name" value={formData.name}  onChange={handleInputChange} />
             </div>
             <div className="group-val">
-              <input type="text" name="email" placeholder="Email" />
+              <input type="text" name="email" placeholder="Email" value={formData.email} onChange={handleInputChange} />
             </div>
             <div className="group-val ct-gr">
               <textarea
                 name="message"
                 placeholder="Message"
-                defaultValue={""}
+                value={formData.message}
+                onChange={handleInputChange}
               />
             </div>
-            <a href="#" className="btn fill" data-text="Send Message">
+            <button className="btn fill" data-text="Send Message">
               Send Message
-            </a>
+            </button>
           </form>
           <div className="alert-success">
             <p>
